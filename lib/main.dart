9c2bd6064789d239a5d9dff69e096c17e04d9ea8
Omar Adel <3maradel010@gmail.com>
2025-07-22 08:29:15 +0300

    Initialize Project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:evently/Screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Evently());
+}
+
+
+class Evently extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (_) => HomeScreen(),
+      },
+      // initialRoute: ,
+    );
+  }
+}
