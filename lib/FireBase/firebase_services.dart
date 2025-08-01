93bd135df58a2ec33a7e86d185212043dfe6a735
Omar Adel <3maradel010@gmail.com>
2025-08-01 17:25:48 +0300

    implemented register & login & signout methods in the firebase services class
    and implemented the get event collection function to cinvert from and to JSON Files

--- lib/FireBase/firebase_services.dart
+++ lib/FireBase/firebase_services.dart
@@ -1,1 +1,55 @@
-class FirebaseServices {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eventlyy/Models/event_model.dart';
+import 'package:eventlyy/Models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseServices {
+  static CollectionReference<UserModel> getUserCollection() => FirebaseFirestore
+      .instance
+      .collection('users')
+      .withConverter<UserModel>(
+        fromFirestore:
+            (docSnapShot, _) => UserModel.fromJson(docSnapShot.data()!),
+        toFirestore: (user, _) => user.toJSON(),
+      );
+
+  static CollectionReference<EventModel> getEventCollection() =>
+      FirebaseFirestore.instance
+          .collection('events')
+          .withConverter<EventModel>(
+            fromFirestore:
+                (docSnapShot, _) => EventModel.fromJSON(docSnapShot.data()!),
+            toFirestore: (event, _) => event.toJSON(),
+          );
+
+  static Future<UserModel> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    UserCredential credential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    UserModel user = UserModel(
+      id: credential.user!.uid,
+      name: name,
+      email: email,
+    );
+    CollectionReference<UserModel> usersCollection = getUserCollection();
+    await usersCollection.doc(user.id).set(user);
+    return user;
+  }
+
+  static Future<UserModel> login({
+    required String email,
+    required String password,
+  }) async {
+    UserCredential credential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    CollectionReference<UserModel> usersCollection = getUserCollection();
+    DocumentSnapshot<UserModel> docSnapShot =
+        await usersCollection.doc(credential.user!.uid).get();
+    return docSnapShot.data()!;
+  }
+
+  static Future<void> logOut() => FirebaseAuth.instance.signOut();
+}
--- lib/Models/user_model.dart
+++ lib/Models/user_model.dart
@@ -2,26 +2,26 @@ class UserModel {
   String id;
   String name;
   String email;
-  List<String> favouriteEvents;
+  // List<String> favouriteEvents;
 
   UserModel({
-    this.id = '',
+    required this.id,
     required this.name,
     required this.email,
-    required this.favouriteEvents,
+    // required this.favouriteEvents,
   });
 
   UserModel.fromJson(Map<String, dynamic> json) : this(
     id: json['id'],
     name: json['name'],
     email: json['email'],
-    favouriteEvents: (json['favouriteEvents'] as List).cast<String>(),
+    // favouriteEvents: (json['favouriteEvents'] as List).cast<String>(),
   );
 
   Map<String, dynamic> toJSON() => {
     'id': id,
     'name': name,
     'email': email,
-    'favouriteEvents': favouriteEvents,
+    // 'favouriteEvents': favouriteEvents,
   };
 }
