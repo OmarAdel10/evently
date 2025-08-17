92c8e86928fae3f297d180a1b2cdc7117e6ea7e4
Omar Adel <3maradel010@gmail.com>
2025-08-17 17:00:09 +0300

    Added Dark Theme To The AppTheme

--- lib/apptheme.dart
+++ lib/apptheme.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class Apptheme {
   static const Color primary = Color(0xFF5669FF);
+  static const Color lightModeBackGround = Color(0xFFF2FEFF);
+  static const Color darkModeBackGround = Color(0xFF101127);
   static const Color white = Color(0xFFF2FEFF);
   static const Color black = Color(0xFF1C1C1C);
   static const Color red = Color(0xFFFF5659);
   static const Color grey = Color(0xFF7B7B7B);
 
   static ThemeData lightTheme = ThemeData(
-    scaffoldBackgroundColor: white,
+    scaffoldBackgroundColor: lightModeBackGround,
 
     appBarTheme: AppBarTheme(
       centerTitle: true,
@@ -104,5 +106,100 @@ class Apptheme {
     ),
   );
 
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: darkModeBackGround,
+
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      elevation: 2,
+      backgroundColor: darkModeBackGround,
+      foregroundColor: primary,
+      titleTextStyle: TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.w400,
+        color: primary,
+      ),
+    ),
+
+    tabBarTheme: TabBarThemeData(
+      dividerColor: Colors.transparent,
+      indicatorColor: Colors.transparent,
+      tabAlignment: TabAlignment.start,
+    ),
+
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: darkModeBackGround,
+      iconSize: 24,
+      shape: CircleBorder(),
+      foregroundColor: white,
+    ),
+
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: darkModeBackGround,
+      selectedItemColor: white,
+      unselectedItemColor: white,
+      selectedLabelStyle: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: white,
+      ),
+      unselectedLabelStyle: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: white,
+      ),
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      prefixIconColor: grey,
+      suffixIconColor: grey,
+      hintStyle: TextStyle(
+        fontSize: 16,
+        color: grey,
+        fontWeight: FontWeight.w500,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: grey, width: 1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: grey, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: grey, width: 1),
+      ),
+    ),
+
+    textTheme: TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        color: white,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: primary,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: black,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: white,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        color: black,
+      ),
+    ),
+  );
 }
