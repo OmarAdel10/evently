f8a886b4521fe9cdd6bdd8f3149ff7529769b93c
Omar Adel <3maradel010@gmail.com>
2025-08-24 15:59:16 +0300

    Fix : Null Error

--- lib/Screens/Home/Tabs/homeTab/home_tab.dart
+++ lib/Screens/Home/Tabs/homeTab/home_tab.dart
@@ -3,11 +3,17 @@ import 'package:eventlyy/Providers/event_provider.dart';
 import 'package:eventlyy/Screens/Home/Events/event_item.dart';
 import 'package:eventlyy/Screens/Home/Tabs/homeTab/home_header.dart';
 import 'package:flutter/material.dart';
-import 'package:giffy_dialog/giffy_dialog.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
   CategoryModel? selectedCategory;
+
   @override
   Widget build(BuildContext context) {
     EventProvider eventProvider = Provider.of<EventProvider>(context);
@@ -15,34 +21,36 @@ class HomeTab extends StatelessWidget {
       children: [
         HomeHeader(selectedCategoryName: selectedCategoryName),
         Expanded(
-          child:
-              eventProvider.dispalyedEvents.isEmpty
-                  ? Column(
-                    children: [
-                      SizedBox(height: MediaQuery.sizeOf(context).height * 0.3),
-                      Lottie.asset('assets/lottie/EmptyBox.json'),
-                      const SizedBox(height: 16,),
-                      Text(
-                        'No ${selectedCategory!.name} Events Found',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                    ],
-                  )
-                  : ListView.separated(
-                    padding: EdgeInsets.all(16),
-                    itemBuilder:
-                        (_, index) => EventItem(
-                          event: eventProvider.dispalyedEvents[index],
-                        ),
-                    separatorBuilder: (_, __) => SizedBox(height: 16),
-                    itemCount: eventProvider.dispalyedEvents.length,
+          child: eventProvider.dispalyedEvents.isEmpty
+              ? Column(
+                  children: [
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.3),
+                    Lottie.asset('assets/lottie/EmptyBox.json'),
+                    const SizedBox(height: 16),
+                    Text(
+                      selectedCategory == null
+                          ? 'No Events Found'
+                          : 'No ${selectedCategory!.name} Events Found',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                )
+              : ListView.separated(
+                  padding: EdgeInsets.all(16),
+                  itemBuilder: (_, index) => EventItem(
+                    event: eventProvider.dispalyedEvents[index],
                   ),
+                  separatorBuilder: (_, __) => SizedBox(height: 16),
+                  itemCount: eventProvider.dispalyedEvents.length,
+                ),
         ),
       ],
     );
   }
 
   void selectedCategoryName(CategoryModel? selectedCategoryName) {
-    selectedCategory = selectedCategoryName;
+    setState(() {
+      selectedCategory = selectedCategoryName;
+    });
   }
 }
